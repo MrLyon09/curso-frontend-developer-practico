8ce73451e93b0945d202d2fd8c828c3f637d4748
Lex <hleon0507@gmail.com>
2022-08-02 22:03:51 -0500

    fusionamos carrito de cmpra

--- main.js
+++ main.js
@@ -2,17 +2,161 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
 
 function toggleDesktopMenu(){
-   
+    
     desktopMenu.classList.toggle('inactive')
 };
 
 
 function toggleMobileMenu(){
-   
+    aside.classList.add('inactive')
     mobileMenu.classList.toggle('inactive')
 };
+
+function toggleCarritoAside(){
+      
+    mobileMenu.classList.add('inactive')
+    aside.classList.toggle('inactive')
+};
+
+
+const productList =[];
+productList.push({
+    name:'Bike',
+    price: 12700,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1200,
+    image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1600,
+    image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg'
+});
+/*productList.push ({
+    name:'Bicycle helmet',
+    price: 1500,
+    image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push ({
+    name:'Seat',
+    price: 300,
+    image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg'
+});
+productList.push ({
+    name:'Tennis Montain Bike',
+    price: 2200,
+    image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
+});
+productList.push ({
+    name:'Sunglasses',
+    price: 800,
+    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602'
+});
+productList.push ({
+    name:'Sunglasses',
+    price: 600,
+    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603'
+});
+productList.push ({
+    name:'Bicycle seat bag',
+    price: 876,
+    image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg'
+
+});
+*/
+// no se debe agregar productos manualmente, sino que se debe crear js y base de datos
+
+//  de aqui tomamos los datos para lo siguiente
+
+/*<div class="product-card">
+        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+        <div class="product-info">
+          <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+        </div>
+      </div> -->
+*/
+function renderProducts(arr) {
+for (product of arr){
+    // product = {name, price, image} -> product.image
+    // primero productCard
+    
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card'); // product-card de la class arriba
+
+    const productImg = document.createElement('img'); // segundo image
+    productImg.setAttribute('src',  product.image);
+
+    // sigue product-info
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    
+
+    // sigue un div VACIO no tiene class ni nombre
+
+    const productInfoDiv = document.createElement('div');
+    
+    
+    
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+
+    const productInfoFigure = document.createElement('figure');
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCard);
+
+}
+}
+renderProducts(productList);
+
+
+// PROCESO COMPLEJO DE MAQUETACION
+// terminado lo anterior debemos devolvernos, 
+/*1. METER LA IMAGEN DENTRO DEL FIGURE
+  2. METER LOS PARRAFOS DE PRECIOS LINEA 88-89 DENTRO DE ESE DIV
+  3. METER AL DIV Y AL FIGURE DENTRO DEL PRODUCT INFO 
+  4. METER AL PRODUCT INFO Y AL IMAGE DENTRO DEL PRODUCT-CARD
+  LO HACEMOS CON EL APPENDCHILD
+ */
+
+    
+
+
+
+
